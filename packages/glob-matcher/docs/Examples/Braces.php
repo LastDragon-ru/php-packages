fdf55f34fdca5bb86bb268680395331a671aac7f
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2026-08-22 23:05:37 +0400

    docs: Rebuild.

--- packages/glob-matcher/docs/Examples/Braces.php
+++ packages/glob-matcher/docs/Examples/Braces.php
@@ -3,11 +3,14 @@
 namespace LastDragon_ru\GlobMatcher\Docs\Examples;
 
 use LastDragon_ru\GlobMatcher\BraceExpander\BraceExpander;
+use LastDragon_ru\GlobMatcher\BraceExpander\Parser\Parser;
 use LastDragon_ru\LaraASP\Dev\App\Example;
 
 use function iterator_to_array;
 
-$expander = new BraceExpander('{a,{0..10..2},c}.txt');
+$pattern  = '{a,{0..10..2},c}.txt';
+$expander = new BraceExpander($pattern);
+$node     = (new Parser())->parse($pattern);
 
 Example::dump(iterator_to_array($expander));
-Example::dump($expander->node);
+Example::dump($node);
